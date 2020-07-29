d3c8c65cd405af2a491cce8433b666139e5503f0
August Aldred <AugustAldred@bpp.com>
2020-07-29 15:50:01 +0100

    fixed timing issue and added match hide/show

--- src/components/Horoscope.js
+++ src/components/Horoscope.js
@@ -5,17 +5,29 @@ function Horoscope(props) {
     const [info, setSigns] = useState({});
     const { sign, date, horoscope } = info;
 
+    const [match, setMatchView] = useState(false)
+    const [loading, setLoading] = useState('')
+
     const { current } = props;
 
     //need error handling
     useEffect(() => {
+        setLoading(true)
         const proxy = 'https://cors-anywhere.herokuapp.com/';
         const target = `http://ohmanda.com/api/horoscope/${current}`
         fetch(proxy + target)
             .then(res => res.json())
-            .then(data => setSigns(data))
+            .then(data => setSigns(data)) //from api
+            .then(loading => setLoading(false))
     }, [current])
 
+    if (loading === true) {
+        return "One moment, searching the stars..."
+    }
+
+    function handleClick() {
+        setMatchView(!match)
+    }
 
     if (current === undefined) {
         return <p>Select your sign</p>;
@@ -23,19 +35,24 @@ function Horoscope(props) {
         return <>
             <h2 className="sign">{sign}</h2>
             <p>{date}</p>
-            <p>{horoscope}</p>
-            <div className="comp-wrapper">
-                {allSigns.map((sign) => {
+            <p class="horoscope">{horoscope}</p>
+            <button onClick={handleClick}>{match === true ? 'Hide matches' : 'View matches'}</button>
+            {match === true ?
+                allSigns.map((sign) => {
                     if (current === sign.name) {
                         return <div key={sign.id} className="comp-container">
-                            <p>Best matches: {sign.best}</p>
-                            <p>Worst matches: {sign.worst}</p>
+                            <p className="best">Best matches: </p>
+                            <p>{sign.best}</p>
+                            <p className="worst">Worst matches: </p>
+                            <p>{sign.worst}</p>
                         </div>
                     } else {
                         return null
                     }
 
-                })}</div>
+                }) :
+                ''
+            }
         </>
     }
 
