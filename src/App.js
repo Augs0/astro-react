9c82e3dd59f285e27c058a392e1ac88131bf9c7f
August <hachigatsu14@outlook.com>
2021-11-15 12:10:40 +0000

    update to working api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
-// import Signs from './components/Signs'
-import Horoscope from './components/Horoscope'
-import allSigns from './assets/data/allsigns'
+import Horoscope from './components/Horoscope';
+import allSigns from './assets/data/allsigns';
 import './App.css';
 
 function App() {
   const [currentSign, setSign] = useState();
-  let current = currentSign
+  let current = currentSign;
 
   return (
-    <div className="container">
-      <div className="black">
+    <div className='container'>
+      <div className='black'>
         <h2>Zodiac Signs</h2>
-        <div className="sign-wrapper">
+        <div className='sign-wrapper'>
           {allSigns.map((sign) => {
-            return <div key={sign.id} className="sign-container">
-              <img className="sign-img" src={sign.image} alt="sign" />
-              <button onClick={() => setSign(`${sign.name}`)} className="sign-name" type="button">{sign.name}
-              </button>
-            </div>
-
-          })}</div>
+            return (
+              <div key={sign.id} className='sign-container'>
+                <img className='sign-img' src={sign.image} alt='sign' />
+                <button
+                  onClick={() => setSign(`${sign.name}`)}
+                  className='sign-name'
+                  type='button'
+                >
+                  {sign.name}
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="white">
+      <div className='white'>
         <h2>Horoscope</h2>
         <Horoscope current={current} />
       </div>
--- src/components/Horoscope.js
+++ src/components/Horoscope.js
@@ -1,65 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import allSigns from '../assets/data/allsigns'
+import React, { useEffect, useState } from 'react';
+import allSigns from '../assets/data/allsigns';
 
 function Horoscope(props) {
-    const [info, setSigns] = useState({});
-    const { sign, date, horoscope } = info;
+  const [info, setSigns] = useState({});
+  const { current_date, description } = info;
 
-    const [match, setMatchView] = useState(false)
-    const [loading, setLoading] = useState('')
+  const [match, setMatchView] = useState(false);
+  const [loading, setLoading] = useState('');
 
-    const { current } = props;
+  const { current } = props;
 
-    //need error handling
-    useEffect(() => {
-        setLoading(true)
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const target = `http://ohmanda.com/api/horoscope/${current}`
-        fetch(proxy + target)
-            .then(res => res.json())
-            .then(data => setSigns(data)) //from api
-            .then(loading => setLoading(false))
-    }, [current])
+  //need error handling
+  useEffect(() => {
+    setLoading(true);
+    // const proxy = 'https://cors-anywhere.herokuapp.com/';
+    // const target = `http://ohmanda.com/api/horoscope/${current}`
+    const url = `https://aztro.sameerkumar.website/?sign=${current}&day=today`;
+    fetch(url, { method: 'POST' })
+      .then((res) => res.json())
+      .then((data) => setSigns(data))
+      .then(() => setLoading(false));
+  }, [current]);
 
-    if (loading === true) {
-        return "One moment, searching the stars..."
-    }
+  if (loading === true) {
+    return 'One moment, searching the stars...';
+  }
 
-    function handleClick() {
-        setMatchView(!match)
-    }
+  function handleClick() {
+    setMatchView(!match);
+  }
 
-    if (current === undefined) {
-        return <p>Select your sign</p>;
-    } else {
-        return <>
-            <h2 className="sign">{sign}</h2>
-            <p>{date}</p>
-            <p className="horoscope">{horoscope}</p>
-            <button onClick={handleClick}>{match === true ? 'Hide matches' : 'View matches'}</button>
-            {match === true ?
-                allSigns.map((sign) => {
-                    if (current === sign.name) {
-                        return <div key={sign.id} className="comp-container">
-                            <p className="best">Best matches: </p>
-                            <p>{sign.best}</p>
-                            <p className="worst">Worst matches: </p>
-                            <p>{sign.worst}</p>
-                        </div>
-                    } else {
-                        return null
-                    }
-
-                }) :
-                ''
-            }
-        </>
-    }
-
+  if (current === undefined) {
+    return <p>Select your sign</p>;
+  } else {
+    return (
+      <>
+        <h2 className='sign'>{current}</h2>
+        <p>{current_date}</p>
+        <p className='horoscope'>{description}</p>
+        <button onClick={handleClick}>
+          {match === true ? 'Hide matches' : 'View matches'}
+        </button>
+        {match === true
+          ? allSigns.map((sign) => {
+              if (current === sign.name) {
+                return (
+                  <div key={sign.id} className='comp-container'>
+                    <p className='best'>Best matches: </p>
+                    <p>{sign.best}</p>
+                    <p className='worst'>Worst matches: </p>
+                    <p>{sign.worst}</p>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })
+          : ''}
+      </>
+    );
+  }
 }
 
 Horoscope.defaultProps = {
-    current: "aries"
-}
+  current: 'aries',
+};
 
-export default Horoscope
+export default Horoscope;
